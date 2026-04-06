13730be86fff2d765e29eb6c47510677582357e5
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-06 15:58:56 +0400

    Task-2:commonEnd was added

--- src/Coding.java
+++ src/Coding.java
@@ -2,13 +2,25 @@ public class Coding {
 
     public static void main(String[] args){
         // task 1
-        int[] nums = {1, 2, 3, 7};
-        System.out.printIn(firstLast6(nums))
+        int[] nums = {6, 1, 2, 3};
+        System.out.println(firstLast6(nums));
+        // task 2
+        int[] a = {1, 2, 3};
+        int[] b = {7, 3};
+
+        System.out.println(commonEnd(a, b));
     }
-    public static boolean firstLast6(int[] num){
+    public static boolean firstLast6(int[] nums){
         if ((nums[0] == 6) || nums[nums.length - 1] ==6) {
             return true;
         }
         return false;
     }
+    public static boolean commonEnd(int[] a, int[] b) {
+        if (a[0] == b[0] || a[a.length - 1] == b[b.length - 1]) {
+            return true;
+        }
+        return false;
+    }
+
 }
