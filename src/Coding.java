fc9052ba977716b89440037fc4c518034a0b7f62
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-06 16:15:19 +0400

    task-5: no23 was added

--- src/Coding.java
+++ src/Coding.java
@@ -18,14 +18,18 @@ public class Coding {
 //            System.out.println(result3[i]);
 //    }
         // task 4
-        int[] a = {1, 2, 3};
-        int[] b = {4, 5, 6};
+//        int[] a = {1, 2, 3};
+//        int[] b = {4, 5, 6};
+//
+//        int[] result4 = middleWay(a, b);
+//
+//        for (int i = 0; i < result4.length; i++) {
+//            System.out.println(result4[i]);
+//        }
+        // task 5
+        int[] nums2 = {4, 2};
 
-        int[] result4 = middleWay(a, b);
-
-        for (int i = 0; i < result4.length; i++) {
-            System.out.println(result4[i]);
-        }
+        System.out.println(no23(nums2));
     }
     public static boolean firstLast6(int[] nums){
         if ((nums[0] == 6) || nums[nums.length - 1] ==6) {
@@ -46,5 +50,11 @@ public class Coding {
     public static int[] middleWay(int[] a, int[] b) {
         int[] middle = {a[1], b[1]};
         return middle;
+    }
+    public static boolean no23(int[] nums) {
+        if (nums[0] != 2 && nums[0] != 3 && nums[1] != 2 && nums[1] != 3) {
+            return true;
+        }
+        return false;
     }
 }
