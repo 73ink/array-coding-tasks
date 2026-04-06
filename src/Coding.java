59c3ab318ac9ac2d263d41935ea905c4def000cf
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-06 16:05:49 +0400

    Task-3:reverse3 was added

--- src/Coding.java
+++ src/Coding.java
@@ -2,13 +2,21 @@ public class Coding {
 
     public static void main(String[] args){
         // task 1
-        int[] nums = {6, 1, 2, 3};
-        System.out.println(firstLast6(nums));
+//        int[] nums = {6, 1, 2, 3};
+//        System.out.println(firstLast6(nums));
         // task 2
-        int[] a = {1, 2, 3};
-        int[] b = {7, 3};
+//        int[] a = {1, 2, 3};
+//        int[] b = {7, 3};
+//
+//        System.out.println(commonEnd(a, b));
+        // task 3
+        int[] nums = {5, 11, 9};
 
-        System.out.println(commonEnd(a, b));
+        int[] result3 = reverse3(nums);
+
+        for (int i = 0; i < result3.length; i++) {
+            System.out.println(result3[i]);
+        }
     }
     public static boolean firstLast6(int[] nums){
         if ((nums[0] == 6) || nums[nums.length - 1] ==6) {
@@ -22,5 +30,8 @@ public class Coding {
         }
         return false;
     }
-
+    public static int[] reverse3(int[] nums) {
+        int[] reversed = {nums[2], nums[1], nums[0]};
+        return reversed;
+    }
 }
